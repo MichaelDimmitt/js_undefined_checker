22dc802ebc76170dcf0c260df0a15e72b02eea3d
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-04-28 10:21:41 -0400

    initial object for testing.

---
+++ test.js
@@ -0,0 +1,27 @@
+const test = {
+  obj1: {
+    str1: 'value1',
+    obj1: {
+      str1: 'value2',
+      arr1: [
+        {arr1: [
+          {str1: 'value3'},
+          {str2: 'value4'},
+          
+          {arr1: [
+           {str1: 'value5',
+           str2: 'value6'}
+          ]}
+        ]},
+        {
+          arr2: [
+            'value7',
+            'value8',
+            'value9',
+            'value10'
+          ],
+        }
+      ]
+    } 
+  }
+}
