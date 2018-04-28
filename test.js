8fea72f08aa3866192d4fb643ed9cedaa70d3de9
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-04-28 13:18:11 -0400

    upload initial tests, failing with expected outputs in mind.

--- test.js
+++ test.js
@@ -44,3 +44,33 @@ function logg([args]) { // dummy values are put into the parameters for readabli
     console.log(JSON.stringify(arguments[i]))
   }
 }
+
+// moving on to implementing the actual test.
+// test_1
+// SHOULD return the value because there is a value there.
+undf_check(test,'test.obj1.obj1.arr1[0].arr1[2].arr1[0].str1')
+
+// test_2
+// SHOULD return empty and not break with an error because previous keys do not have values. But that should be caught early.
+undf_check(test,'test.obj1.obj1.arr1[0].arr1[2].arr1[0].str1.foo.bas.blah')
+
+// test_3
+// total junk ... should return empty and not break.
+undf_check(object, 'test.blah.whoa.frog.be.dragon')
+
+// I want to send the logg ... an array of values.
+// I need to wrap the function logg parameters around an array.
+
+// the below did not work.
+
+// function logg(funct(args)) { // these args are never used.
+// console.log(arr)
+// console.log(args)
+//   for (var i = 0; i < arr.length; i++) {
+//     console.log(JSON.stringify(arr[i]))
+//   }
+// }
+//
+// function funct(args) { // these args are never used.
+//   return uargs]
+// }
