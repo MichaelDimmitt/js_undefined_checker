2fb91619bbd1a4fda1394175dbb9073b82e701f8
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-04-28 17:35:41 -0400

    presenting some scenerios.

--- test.js
+++ test.js
@@ -37,7 +37,6 @@ logg(test.obj1.obj1.arr1[0].arr1[2].arr1[0].str2)
   logg(test.obj1.obj1.arr1[1].arr2[2])
   logg(test.obj1.obj1.arr1[1].arr2[3])
 
-
 function logg([args]) { // dummy values are put into the parameters for readablility purposes.
  var arguments = logg.arguments; // arguements passes all of the parameters by value.
   for (let i = 0; i < arguments.length; i++) {
@@ -45,32 +44,31 @@ function logg([args]) { // dummy values are put into the parameters for readabli
   }
 }
 
-// moving on to implementing the actual test.
+function get_if_exist(str) {
+  try { return eval(str) }
+  catch(e) { return {} }
+} 
+let testp1 = get_if_exist('test.obj1.obj1.arr1[1].arr2[3]')
+let testp2 = get_if_exist('test.obj1.obj1.arr1[7].arr2[3]')
+console.log('implementing test1', testp1, testp2)
+
+function index(obj,i) {return obj[i] || {}}
+let testy1 = 'test.obj1.obj1.arr1[1].arr2[3]'.split('.').reduce(index, test)
+let testy2 = 'test.obj1.obj1.arr1[3].arr2[3]'.split('.').reduce(index, test)
+let testy3 = 'test.obj5.obj1.arr1[1].arr2[3]'.split('.').reduce(index, test)
+console.log('implementing test3', testy1, testy2, testy3)
+
 // test_1
 // SHOULD return the value because there is a value there.
-undf_check(test,'test.obj1.obj1.arr1[0].arr1[2].arr1[0].str1')
+// undf_check(test,'test.obj1.obj1.arr1[0].arr1[2].arr1[0].str1')
 
 // test_2
 // SHOULD return empty and not break with an error because previous keys do not have values. But that should be caught early.
-undf_check(test,'test.obj1.obj1.arr1[0].arr1[2].arr1[0].str1.foo.bas.blah')
+// undf_check(test,'test.obj1.obj1.arr1[0].arr1[2].arr1[0].str1.foo.bas.blah')
 
 // test_3
 // total junk ... should return empty and not break.
-undf_check(object, 'test.blah.whoa.frog.be.dragon')
+// undf_check(object, 'test.blah.whoa.frog.be.dragon')
 
 // I want to send the logg ... an array of values.
 // I need to wrap the function logg parameters around an array.
-
-// the below did not work.
-
-// function logg(funct(args)) { // these args are never used.
-// console.log(arr)
-// console.log(args)
-//   for (var i = 0; i < arr.length; i++) {
-//     console.log(JSON.stringify(arr[i]))
-//   }
-// }
-//
-// function funct(args) { // these args are never used.
-//   return uargs]
-// }
